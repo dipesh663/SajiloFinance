75df4aac313fd04a1748f556b4b7ff3d7220275e
dipesh663 <dipeshthapa663@gmail.com>
2026-04-26 18:05:44 +0545

    Introduces standardized API response format
    Creates a generic `ApiResponse` class to ensure consistent response structures across API endpoints. This class includes fields for message, status code, data, and authentication tokens.
    
    Updates the user registration endpoint to return `ResponseEntity<ApiResponse>`, providing clear success or error messages along with appropriate HTTP status codes, enhancing client communication and error handling.

--- sajilo_finance/src/main/java/com/expensetracker/sajilo_finance/controller/userController.java
+++ sajilo_finance/src/main/java/com/expensetracker/sajilo_finance/controller/userController.java
@@ -1,13 +1,15 @@
 package com.expensetracker.sajilo_finance.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.expensetracker.sajilo_finance.apiResponse.ApiResponse;
 import com.expensetracker.sajilo_finance.model.dto.UserDto;
-import com.expensetracker.sajilo_finance.model.entity.User;
 import com.expensetracker.sajilo_finance.service.UserService;
 
 
@@ -21,8 +23,19 @@ public class userController {
     UserService userService;
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody UserDto userDto){
-        return  userService.register(userDto);
+    public ResponseEntity<ApiResponse> registerUser(@RequestBody UserDto userDto){
+          userService.register(userDto);
+          try {
+              ApiResponse apiResponse = ApiResponse.builder().message("Successfully Created")
+                                        .statusCode(HttpStatus.OK.value()).build();
+
+              return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+
+          } catch (Exception e) {
+            ApiResponse apiResponse = ApiResponse.builder().message("Failed")
+                    .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiResponse);
+          }
     }
    
     
